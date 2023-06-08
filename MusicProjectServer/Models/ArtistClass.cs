51513da99414fb8e65d843d81bef5a543125ca1e
noam1134 <90392508+noam1134@users.noreply.github.com>
2023-06-08 15:32:03 +0300

    Create ArtistClass.cs

---
+++ MusicProjectServer/Models/ArtistClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MusicProjectServer.Models
+{
+	public class ArtistClass
+	{
+        private string Name { get; set; }
+		private int Id { get; set; }
+
+        public ArtistClass()
+		{
+
+		}
+    }
+}
+
