9252ca3efcbfc927d92dae9bc543e7d7d3df0369
CSgroup16 <131266999+RonAzar@users.noreply.github.com>
2023-06-08 15:40:26 +0300

    Create Song.cs

---
+++ MusicProjectServer/Models/Song.cs
@@ -0,0 +1,23 @@
+﻿namespace MusicProjectServer.Models
+{
+    public class Song
+    {
+        private int songId;
+        private string songName;
+        private string text;
+        private string link;
+
+        public Song(int songId, string songName, string text, string link)
+        {
+            this.SongId = songId;
+            this.SongName = songName;
+            this.Text = text;
+            this.Link = link;
+        }
+
+        public int SongId { get => songId; set => songId = value; }
+        public string SongName { get => songName; set => songName = value; }
+        public string Text { get => text; set => text = value; }
+        public string Link { get => link; set => link = value; }
+    }
+}
