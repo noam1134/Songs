7169472754e33bcd2fef465f1d35f8ab96b3ff25
noam1134 <noamchuri96@gmail.com>
2023-07-09 20:58:24 +0300

    Update SongsController.cs

--- MusicProjectServer/Controllers/SongsController.cs
+++ MusicProjectServer/Controllers/SongsController.cs
@@ -13,7 +13,10 @@ namespace MusicProjectServer.Controllers
         [HttpGet]
         public List<Song> Get()
         {
-            return new List<Song> {};
+            //ArtistClass ron = new ArtistClass("Ron Azar",1,69);
+            Song song_1 = new Song(new ArtistClass("Ron Azar",1,69), 1, "Nuni", "Lehem Havita!", "https:" + "//" + "youtu.be" + "/" + "_RFUj-sGCp8");
+            Song song_2 = new Song(new ArtistClass("Ron Azar", 1, 69), 2, "Poki", "Poki Pok!", "https:" + "//" + "youtu.be" + "/" + "_RFUj-sGCp8");
+            return new List<Song> { song_1, song_2 };
         }
 
         // GET api/<SongsController>/5
