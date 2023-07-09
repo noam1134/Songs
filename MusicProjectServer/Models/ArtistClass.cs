d848f0a64588a9ca362688ecdd9b3e22cb78891e
noam1134 <noamchuri96@gmail.com>
2023-07-09 19:54:45 +0300

    updated artist

--- MusicProjectServer/Models/ArtistClass.cs
+++ MusicProjectServer/Models/ArtistClass.cs
@@ -5,14 +5,17 @@ namespace MusicProjectServer.Models
 	{
         private string Name { get; set; }
 		private int Id { get; set; }
+        private int Popularity { get; set; }
 
         public ArtistClass()
 		{
 			Name = "";
+            Popularity = 0; 
 		}
 
-        public ArtistClass(string name, int id)
+        public ArtistClass(string name, int id, int popularity)
         {
+            Popularity = popularity;
             Name = name;
             Id = id;
         }
--- MusicProjectServer/Models/Song.cs
+++ MusicProjectServer/Models/Song.cs
@@ -3,27 +3,31 @@
     public class Song
     {
         private int songId;
-        private string songName;
-        private string text;
+        private string songName; //saved in excell as 'song'
+        private string lyrics; //saved in excell as 'text'
         private string link;
+        private ArtistClass artist;
 
         public Song()
         {
             SongName = "";
-            Text = "";
+            Lyrics = "";
             Link = "";
+            Artist = new ArtistClass();
         }
-        public Song(int songId, string songName, string text, string link)
+        public Song(ArtistClass artist,int songId, string songName, string text, string link)
         {
+            Artist = artist;
             SongId = songId;
             SongName = songName;
-            Text = text;
+            Lyrics = text;
             Link = link;
         }
 
+        public ArtistClass Artist { get => artist; set => artist = value; }
         public int SongId { get => songId; set => songId = value; }
         public string SongName { get => songName; set => songName = value; }
-        public string Text { get => text; set => text = value; }
+        public string Lyrics { get => lyrics; set => lyrics = value; }
         public string Link { get => link; set => link = value; }
     }
 }
