d081db3a6d6ddd26101fe7c44127690f3ac2ec94
noam1134 <noamchuri96@gmail.com>
2023-07-17 12:19:50 +0300

    Update create Artists table.sql

--- DataFiles/create Artists table.sql
+++ DataFiles/create Artists table.sql
@@ -2,7 +2,7 @@ drop table if exists Songs
 drop table if exists Artists
 go
 create table Artists(
-	artistId int primary key,
+	artistId int identity primary key,
 	artistName varchar(30),
 	popularity int
 )
